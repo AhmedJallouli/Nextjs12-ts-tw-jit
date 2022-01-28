5636be2cc5630cefe4858a7303716649c0799d3c
Amir HWL <641mirhaouala@gmail.com>
2022-01-28 07:10:56 +0100

    fixes components path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+// https://tailwindcss.com/docs/configuration
+
 module.exports = {
   mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
