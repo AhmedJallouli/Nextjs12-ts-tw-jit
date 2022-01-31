f189ebe682dfbc563119ebdfeb149c922de823c6
Amir HWL <641mirhaouala@gmail.com>
2022-01-31 10:20:11 +0100

    fixes Documentation component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,11 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <MainLayout>
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <Documentation header={'Documentation body'} body={'qdssq'} />
-          </a>
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <Documentation header={'Learn body'} body={'qdssq'} />
-          </a>
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <Documentation header={'Learn body'} body={'qdssq'} />
-          </a>
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <Documentation header={'Learn body'} body={'qdssq'} />
-          </a>
+          <Documentation header={'Documentation body'} body={'qdssq'} />
+
+          <Documentation header={'Learn body'} body={'qdssq'} />
+          <Documentation header={'Learn body'} body={'qdssq'} />
+          <Documentation header={'Learn body'} body={'qdssq'} />
         </div>
       </MainLayout>
     </div>
--- src/components/dashboard/Documentation.tsx
+++ src/components/dashboard/Documentation.tsx
@@ -7,9 +7,12 @@ type DocumentationInterface = {
 
 export default function Documentation(props: DocumentationInterface) {
   return (
-    <div>
+    <a
+      href="https://nextjs.org/docs"
+      className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+    >
       <h3 className="text-2xl font-bold">{props.header} &rarr;</h3>
       <p className="mt-4 text-xl">{props.body}</p>
-    </div>
+    </a>
   )
 }
